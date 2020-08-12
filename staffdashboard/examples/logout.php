dad206e20b2024939a3b19264fe5366768cbb47f
LAPTOP-48T99C5E\Atifah Farzana <atifahfarzanahalim@gmail.com>
2020-08-13 05:05:24 +0800

    -------------minor changes

---
+++ staffdashboard/examples/logout.php
@@ -0,0 +1,6 @@
+<?php
+session_start(); 
+session_destroy();
+header('Location: master_roomesquare/Homepage/index.html');
+?>
+	
---
+++ staffdashboard/examples/update.php
@@ -0,0 +1,39 @@
+<?php
+     session_start();
+	if (isset($_POST['updateButton'])) {
+		$con = mysqli_connect("localhost","hotel","hotel","hoteldb");
+        if (!$con) {
+            echo  mysqli_connect_error();
+            exit;
+        }
+
+        
+        $sql = "UPDATE `customer` SET 
+		`custname` = '".$_POST['fullname']."', 
+		`custemail` = '".$_POST['email']."', 
+		`custusername` = '".$_POST['username']."', 
+		`custphoneno` = '".$_POST['phone']."'
+        WHERE `customer`.`custusername` = '".$_SESSION['username']."'";
+        $result = mysqli_query($con, $sql);
+		
+		echo $sql;
+		
+		$sql2 = "UPDATE `users` SET 
+		`username` = '".$_POST['username']."'
+        WHERE `username` = '".$_SESSION['username']."'";
+        $result2 = mysqli_query($con, $sql2);
+
+        echo $sql2;
+		
+		//update session
+		$_SESSION['username'] = $_POST['username'];
+
+      
+		print_r($_SESSION);
+
+    }
+?>
+
+
+
+
